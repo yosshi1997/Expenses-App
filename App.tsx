e7036167677b2b6c4973893c1367832a9b1c2e89
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-12 21:11:01 +0900

    Install ESLint

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
+import { JSX } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = (): JSX.Element => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
