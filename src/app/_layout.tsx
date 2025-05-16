31b023c924901579e17154a30d991ee8c5c618dc
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-16 17:43:59 +0900

    Install expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Slot } from "expo-router"
+import { JSX } from "react"
+
+const Layout = (): JSX.Element => {
+    return <Slot />
+}
+
+export default Layout
---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { JSX } from "react"
+import { View, Text } from "react-native"
+
+const Index = (): JSX.Element => {
+    return (
+        <View>
+            <Text>
+                Hello World!
+            </Text>
+        </View>
+    )
+}
+
+export default Index
