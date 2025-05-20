b916e6c61f5a47133644c93a8cefc4ad4d4da622
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-20 22:59:47 +0900

    Create UserList File and setting "Redirect" in index.tsx

---
+++ src/app/User/UserList.tsx
@@ -0,0 +1,47 @@
+import { JSX } from "react"
+import { View, StyleSheet } from "react-native"
+
+import Header from "../../conponents/Header"
+import UserListItem from "../../conponents/UserListItem"
+import AddButton from "../../conponents/AddButton"
+import NewUserButton from "../../conponents/NewUserButton"
+import FooterButton from "../../conponents/FooterButton"
+
+const UserList = (): JSX.Element => {
+    return (
+        <View style={styles.container}>
+            {/* header */}
+            <Header />
+
+            {/* User List Scroll */}
+            <View>
+                <UserListItem updateTime="2025.04.30 13:14 更新">Yoshimasa</UserListItem>
+                <UserListItem updateTime="2025.04.30 14:14 更新">Aya</UserListItem>
+                <UserListItem updateTime="2025.04.30 15:14 更新">Family</UserListItem>
+            </View>
+
+            {/* rotate={true}なら45度回転 */}
+            <AddButton rotate={true} />
+            {/* visible={true}なら表示 */}
+            <NewUserButton visible={true} />
+            <FooterButton />
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgb(255, 255, 255)"
+    }
+})
+
+export default UserList
+
+
+{/* フォント編集
+    headerTitle
+    userName
+    newUserButtonText
+    footerButtonText */ }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,46 +1,8 @@
+import { Redirect } from "expo-router"
 import { JSX } from "react"
-import { View, StyleSheet } from "react-native"
 
-import Header from "../conponents/Header"
-import UserListItem from "../conponents/UserListItem"
-import AddButton from "../conponents/AddButton"
-import NewUserButton from "../conponents/NewUserButton"
-import FooterButton from "../conponents/FooterButton"
-
-const Index = (): JSX.Element => {
-    return (
-        <View style={styles.container}>
-            {/* header */}
-            <Header />
-
-            {/* User List Scroll */}
-            <View>
-                <UserListItem updateTime="2025.04.30 13:14 更新">Yoshimasa</UserListItem>
-                <UserListItem updateTime="2025.04.30 14:14 更新">Aya</UserListItem>
-                <UserListItem updateTime="2025.04.30 15:14 更新">Family</UserListItem>
-            </View>
-
-            {/* rotate={true}なら45度回転 */}
-            <AddButton rotate={true} />
-            <NewUserButton visible={true} />
-            <FooterButton />
-
-        </View>
-    )
+const index = (): JSX.Element => {
+    return <Redirect href="User/UserList" />
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "rgb(255, 255, 255)"
-    }
-})
-
-export default Index
-
-
-{/* フォント編集
-    headerTitle
-    userName
-    newUserButtonText
-    footerButtonText */ }
+export default index
