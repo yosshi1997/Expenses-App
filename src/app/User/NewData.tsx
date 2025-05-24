630de76866e55ff35de7b5a4b195f56be95ac6d1
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-25 00:23:12 +0900

    Create conponent NumberInput.tsx and Edit NewData.tsx

--- src/app/User/NewData.tsx
+++ src/app/User/NewData.tsx
@@ -2,10 +2,9 @@ import { JSX } from "react"
 import { Text, View, StyleSheet } from "react-native"
 
 import Header from "../../conponents/Header"
-import AddButton from "../../conponents/AddButton"
-import NewUserButton from "../../conponents/NewUserButton"
 import FooterButton from "../../conponents/FooterButton"
 import StringInput from "../../conponents/StringInput"
+import NumberInput from "../../conponents/NumberInput"
 
 const NewData = (): JSX.Element => {
     return (
@@ -17,43 +16,13 @@ const NewData = (): JSX.Element => {
 
             <StringInput label="User" defaultValue="Yoshimasa"></StringInput>
 
+            <NumberInput label="Year">2025</NumberInput>
 
-            <View style={styles.inputLabel}>
-                <Text style={styles.inputLabelText}>
-                    Year
-                </Text>
-            </View>
-            <View style={styles.inputNumber}>
-                <Text style={styles.inputText}>
-                    2025
-                </Text>
-            </View>
-
-
-            <View style={styles.inputLabel}>
-                <Text style={styles.inputLabelText}>
-                    Month
-                </Text>
-            </View>
-            <View style={styles.inputNumber}>
-                <Text style={styles.inputText}>
-                    12
-                </Text>
-            </View>
+            <NumberInput label="Month">12</NumberInput>
 
             <StringInput label="Category">SBI銀行</StringInput>
 
-
-            <View style={styles.inputLabel}>
-                <Text style={styles.inputLabelText}>
-                    Income/Expense
-                </Text>
-            </View>
-            <View style={styles.inputNumber}>
-                <Text style={styles.inputText}>
-                    194129
-                </Text>
-            </View>
+            <NumberInput label="Income/Expense">194129</NumberInput>
 
             <View style={styles.noteText}>
                 <Text>
@@ -69,41 +38,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "rgb(255, 255, 255)"
-    },
-    inputLabel: {
-        width: "100%",
-        height: 40,
-        backgroundColor: "rgb(180,180,180)",
-        justifyContent: "center",
-        alignItems: "center",
-        marginVertical: 16
-    },
-    inputLabelText: {
-        fontSize: 24,
-        fontWeight: "bold"
-    },
-    inputString: {
-        height: 40,
-        width: "90%",
-        marginHorizontal: "5%",
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'rgb(0, 0, 0)',
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    inputNumber: {
-        height: 40,
-        width: "90%",
-        marginHorizontal: "5%",
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'rgb(0, 0, 0)',
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    inputText: {
-        fontSize: 16
     },
     noteText: {
         alignItems: "center"
