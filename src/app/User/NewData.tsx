04dcc61c3197ce65331bb1fef3e0b3020407c8af
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-24 23:42:01 +0900

    Create Structure and Styles of NewData.tsx

--- src/app/User/NewData.tsx
+++ src/app/User/NewData.tsx
@@ -14,17 +14,69 @@ const NewData = (): JSX.Element => {
             <Header signUpVisible={false} logoutVisible={false} backVisible={true} editVisible={false}>New Data</Header>
 
             <View style={styles.inputLabel}>
-                <View style={styles.inputString}>
-
-                </View>
+                <Text style={styles.inputLabelText}>
+                    User
+                </Text>
             </View>
+            <View style={styles.inputString}>
+                <Text style={styles.inputText}>
+                    UserName
+                </Text>
+            </View>
+
 
             <View style={styles.inputLabel}>
-                <View style={styles.inputNumber}>
-
-                </View>
+                <Text style={styles.inputLabelText}>
+                    Year
+                </Text>
+            </View>
+            <View style={styles.inputNumber}>
+                <Text style={styles.inputText}>
+                    2025
+                </Text>
             </View>
 
+
+            <View style={styles.inputLabel}>
+                <Text style={styles.inputLabelText}>
+                    Month
+                </Text>
+            </View>
+            <View style={styles.inputNumber}>
+                <Text style={styles.inputText}>
+                    12
+                </Text>
+            </View>
+
+
+            <View style={styles.inputLabel}>
+                <Text style={styles.inputLabelText}>
+                    Category
+                </Text>
+            </View>
+            <View style={styles.inputString}>
+                <Text style={styles.inputText}>
+                    SBI銀行
+                </Text>
+            </View>
+
+
+            <View style={styles.inputLabel}>
+                <Text style={styles.inputLabelText}>
+                    Income/Expense
+                </Text>
+            </View>
+            <View style={styles.inputNumber}>
+                <Text style={styles.inputText}>
+                    194129
+                </Text>
+            </View>
+
+            <View style={styles.noteText}>
+                <Text>
+                    ※収入は正の値、支出は負の値で入力
+                </Text>
+            </View>
 
             <FooterButton>Add New Data</FooterButton>
         </View>
@@ -36,13 +88,42 @@ const styles = StyleSheet.create({
         backgroundColor: "rgb(255, 255, 255)"
     },
     inputLabel: {
-
+        width: "100%",
+        height: 40,
+        backgroundColor: "rgb(180,180,180)",
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 16
+    },
+    inputLabelText: {
+        fontSize: 24,
+        fontWeight: "bold"
     },
     inputString: {
-
+        height: 40,
+        width: "90%",
+        marginHorizontal: "5%",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'rgb(0, 0, 0)',
+        justifyContent: "center",
+        alignItems: "center"
     },
     inputNumber: {
-
+        height: 40,
+        width: "90%",
+        marginHorizontal: "5%",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'rgb(0, 0, 0)',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputText: {
+        fontSize: 16
+    },
+    noteText: {
+        alignItems: "center"
     }
 })
 
