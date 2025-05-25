add4dea555ca80a517fb1bb9e4c82d1ecec774f2
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-05-25 10:31:46 +0900

    Edit NewUser.tsx

--- src/app/User/NewUser.tsx
+++ src/app/User/NewUser.tsx
@@ -5,6 +5,8 @@ import Header from "../../conponents/Header"
 import AddButton from "../../conponents/AddButton"
 import NewUserButton from "../../conponents/NewUserButton"
 import FooterButton from "../../conponents/FooterButton"
+import StringInput from "../../conponents/StringInput"
+import NumberInput from "../../conponents/NumberInput"
 
 const NewUser = (): JSX.Element => {
     return (
@@ -13,6 +15,9 @@ const NewUser = (): JSX.Element => {
             {/* header */}
             <Header signUpVisible={false} logoutVisible={false} backVisible={true} editVisible={false}>New User</Header>
 
+            <StringInput label="User Name">Yoshimasa</StringInput>
+
+            <NumberInput label="Initial Value">50000</NumberInput>
 
 
             <FooterButton>Add New User</FooterButton>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router"
 import { JSX } from "react"
 
 const index = (): JSX.Element => {
-    return <Redirect href="User/Login" />
+    return <Redirect href="User/NewUser" />
 }
 
 export default index
