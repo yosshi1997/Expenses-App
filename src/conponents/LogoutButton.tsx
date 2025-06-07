616b5450e0467f46e636dd794bacc29a4fcf7ddf
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-06-07 20:36:31 +0900

    Add Logout function

--- src/conponents/LogoutButton.tsx
+++ src/conponents/LogoutButton.tsx
@@ -1,19 +1,32 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
-import { Link } from "expo-router"
+import { router } from "expo-router"
+import { signOut } from "firebase/auth"
+import { auth } from "../config"
+
+
+const handlePress = (): void => {
+    signOut(auth)
+        .then(() => {
+            router.replace("/User/Login")
+            console.log("Logout success")
+        })
+        .catch(() => {
+            Alert.alert("ログアウトに失敗しました。")
+        })
+}
 
 interface Props {
     logoutVisible?: boolean
 }
+
 const LogoutButton = ({ logoutVisible = false }: Props): JSX.Element => {
     return (
 
         < View>
-            <Link href="/User/Login" asChild replace>
-                <TouchableOpacity>
-                    <Text style={[styles.headerLogoutButton, logoutVisible && styles.logoutVisible]}>Logout</Text>
-                </TouchableOpacity>
-            </Link>
+            <TouchableOpacity onPress={handlePress}>
+                <Text style={[styles.headerLogoutButton, logoutVisible && styles.logoutVisible]}>Logout</Text>
+            </TouchableOpacity>
         </View >
     )
 }
