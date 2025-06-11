bd09076305e62b994508f082a5b233bbef1664c3
fukuta <164353088+yosshi1997@users.noreply.github.com>
2025-06-11 22:15:32 +0900

    Create NewUser add function

--- src/app/User/NewUser.tsx
+++ src/app/User/NewUser.tsx
@@ -7,10 +7,28 @@ import AddButton from "../../conponents/AddButton"
 import FooterButton from "../../conponents/FooterButton"
 import BackButton from "../../conponents/BackButton"
 import Label from "../../conponents/Label"
+import { collection, addDoc, Timestamp } from "firebase/firestore"
+import { db, auth } from "../../config"
+import { useState } from "react"
 
-const handlePress = (): void => {
+const handlePress = (userName: string, userInitial: string): void => {
     //Add New User
-    router.replace("/User/UserList")
+    if (auth.currentUser === null) { return }
+
+    const ref = collection(db, `users/${auth.currentUser?.uid}/userList/${userName}/categories/income0/data`)
+
+    addDoc(ref, {
+        userInitial,
+        addDate: Timestamp.fromDate(new Date())
+    })
+        .then((docRef) => {
+            console.log("success", docRef.id)
+            router.replace("/User/UserList")
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
 }
 
 const handleBack = (): void => {
@@ -20,30 +38,32 @@ const handleBack = (): void => {
 
 
 const NewUser = (): JSX.Element => {
+    const [userName, setUserName] = useState("")
+    const [userInitial, setUserInitial] = useState("")
+
     return (
         <View style={styles.container}>
 
             {/* header */}
             <Header>NewUser</Header>
             <BackButton backVisible={true} onPress={handleBack}></BackButton>
-
             <Label>User Name</Label>
             <View style={styles.inputString}>
-                <TextInput>
-                    Yoshimasa
+                <TextInput value={userName} onChangeText={(text) => { setUserName(text) }}>
+
                 </TextInput>
             </View>
 
             <Label>Initial Value</Label>
             <View style={styles.inputString}>
-                <TextInput>
-                    50000
+                <TextInput value={userInitial} onChangeText={(text) => { setUserInitial(text) }}>
+
                 </TextInput>
             </View>
 
             <AddButton rotate={false}>∨</AddButton>
 
-            <FooterButton onPress={handlePress}>Add New User</FooterButton>
+            <FooterButton onPress={() => { handlePress(userName, userInitial) }}>Add New User</FooterButton>
         </View>
     )
 }
